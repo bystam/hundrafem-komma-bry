db520254bbe1f77456c50adc640f17b3ba37e28f
Fredrik Bystam <fredrik.bystam@gmail.com>
2013-06-18 21:29:07 +0200

    webpage done

--- web.js
+++ web.js
@@ -1,14 +1,28 @@
 var express = require('express');
-
+var stylus = require('stylus');
 var app = express();
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib())
+}
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
+app.use(express.logger('dev'));
+app.use(stylus.middleware(
+	{ src: __dirname + '/public'
+	, compile: compile
+	}
+));
 
 app.get('/', function(req, res) {
-	res.render('index');
+	res.render('index',
+		{ title : 'Radio' }
+	);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on port: " + port);
 });
