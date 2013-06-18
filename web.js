5241f751b82f752058e33139b3e7bd9e2b2b66c4
Fredrik Bystam <fredrik.bystam@gmail.com>
2013-06-18 23:19:07 +0200

    webpage done

--- web.js
+++ web.js
@@ -1,14 +1,18 @@
-var express = require('express');
-var stylus = require('stylus');
-var nib = require('nib');
-var app = express();
+var express = require('express')
+	app = express(),
+	server = require('http').createServer(app),
+	io = require('socket.io').listen(server),
+	stylus = require('stylus'),
+	nib = require('nib');
 
+// setup .styl to .css compilation
 function compile(str, path) {
   return stylus(str)
     .set('filename', path)
     .use(nib())
 }
 
+// express app configuring
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.logger('dev'));
@@ -17,16 +21,24 @@ app.use(stylus.middleware(
 	, compile: compile
 	}
 ));
+app.use(express.static(__dirname + '/public'))
 
+// setup root route
 app.get('/', function(req, res) {
 	res.render('index',
 		{ title : 'Radio' }
 	);
 });
 
-app.use(express.static(__dirname + '/public'))
+// setup socket.io echoing
+io.sockets.on('connection', function (socket) {
+	socket.on('data', function(data) {
+		socket.write(data);
+	});
+});
 
+// start server listening
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
+server.listen(port, function() {
 	console.log("Listening on port: " + port);
 });
