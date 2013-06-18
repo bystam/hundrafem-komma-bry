1f38bb15bae93fc74ded01881c3109f1c87ceb7e
Fredrik Bystam <fredrik.bystam@gmail.com>
2013-06-18 21:10:03 +0200

    webpage done

--- web.js
+++ web.js
@@ -8,5 +8,7 @@ app.get('/', function(req, res) {
 	res.render('index');
 });
 
-var port = process.env.PORT || 3000;
-app.listen(port);
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
