8da80e1a7a74ed96d49cf6ae1ac3e1bb4a3eb3fc
Jens Arvidsson <jensarv@gmail.com>
2014-08-09 14:00:29 +0200

    deleted auth

--- auth.js
+++
@@ -1,6 +0,0 @@
-exports.google_username = "hundrafem.komma.bry";
-exports.google_password = "delicatodumle";
-exports.google_form_key = "1qn-drV9vpNfrLLP6PJu-xV7gJ6WUkqI1GFJ_VTptZm0";
-
-exports.nada_user = "wat";
-exports.nada_password = "literallynoidea";
